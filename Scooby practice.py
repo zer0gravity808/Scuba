efbbe1990d94da2003a3c62ed911efa62e8d6d27
zer0gravity808 <81837211+zer0gravity808@users.noreply.github.com>
2021-04-23 16:07:21 -0500

    Add files via upload

---
+++ Scooby practice.py
@@ -0,0 +1,5 @@
+print('Scuba Expedition application \n')
+
+Locations = input('Please slect a location from the following:Austrailia,Belize or Fiji \n')
+Number_of_people = input('Please input how many are going on the trip \n')
+if Locations = 
